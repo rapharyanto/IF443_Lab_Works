ecf2662199c8ddeb81b9e4be9c15e8fb46bc9b7c
Rap <tinydiamondzgt@gmail.com>
2026-02-06 15:36:49 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week01
+
+fun main () {
+    var name: String = "Rafi Athallah Ahmad Haryanto"
+    var score: Int = 100
+
+    println("Nama: " + name + ", Score: " + score)
+}
